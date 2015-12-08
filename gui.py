4aac4ce93c4da465554b62c9b140750a0f8c8ddf
LaurentRDC <laurent.decotret@outlook.com>
2015-12-08 13:50:28 -0500

    Created the TIFF image viewer

--- gui.py
+++ gui.py
@@ -16,12 +16,40 @@ else:
     from PyQt4 import QtGui, QtCore
 
 class ImageViewer(FigureCanvas):
-    
-    def __init__(self):
+    """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
         
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        # We want the axes cleared every time plot() is called
+        self.axes.hold(False)
+
+        self.compute_initial_figure()
+
+        #
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self,
+                                   QtGui.QSizePolicy.Expanding,
+                                   QtGui.QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+
+    def compute_initial_figure(self):
         pass
     
-    
+class TIFFImageViewer(ImageViewer):
+    """ For plotting the raw TIFF images """
+
+    def compute_initial_figure(self, image):
+        """
+        Parameters
+        ----------
+        image - ndarray, shape (N,N)
+            TIFF file imported into a ndarray using PIL.Image.open()
+        """
+        self.axes.imshow(image)
     
 class UEDpowder(QtGui.QMainWindow):
     """
