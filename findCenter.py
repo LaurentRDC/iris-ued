bf77e905c959fc9b65f6a5fb746bcd6208000143
msternie <msternie@gmail.com>
2015-12-08 13:35:49 -0500

    added file

---
+++ findCenter.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+findCenter
+
+
+"""
+
+import numpy as n
+from PIL import Image
+
+
+
+
+filename = ''#temp glob var
+im = n.array(Image.open(filename))#temp glob var
+
+
+
+def findCenter(xg, yg, im):
+    
+    return None
+
