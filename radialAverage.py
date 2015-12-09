bde5ed4b3258181cbd09997d1181feb875ba8879
LaurentRDC <laurent.decotret@outlook.com>
2015-12-09 16:53:26 -0500

    Built documentation

--- radialAverage.py
+++ radialAverage.py
@@ -1,4 +1,23 @@
 # -*- coding: utf-8 -*-
 
-def radialAverage(image):
-    pass
+import numpy as n
+from PIL.Image import open 
+
+filename = 'C:\Users\Laurent\Dropbox\Powder\VO2\NicVO2\NicVO2_2.tif
+
+def radialAverage(image, center = [0,0]):
+    """
+    This function returns a radially-averaged pattern computed from a TIFF image.
+    
+    Parameters
+    ----------
+    image : ndarray, shape(N,N)
+    
+    center : array-like, shape (2,)
+        [x,y] coordinates of the center (in pixels)
+    
+    Returns
+    -------
+    [s, pattern] : list of ndarrays, shapes (M,)
+    """
+    return
