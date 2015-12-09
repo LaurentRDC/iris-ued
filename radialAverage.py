86b6c196391736a4ab5c936674af4b2fe6e4ad07
LaurentRDC <laurent.decotret@outlook.com>
2015-12-09 17:05:14 -0500

    Function-building

--- radialAverage.py
+++ radialAverage.py
@@ -3,7 +3,8 @@
 import numpy as n
 from PIL.Image import open 
 
-filename = 'C:\Users\Laurent\Dropbox\Powder\VO2\NicVO2\NicVO2_2.tif
+filename = 'C:\Users\Laurent\Dropbox\Powder\VO2\NicVO2\NicVO2_2.tif'
+im = n.array(open(filename))
 
 def radialAverage(image, center = [0,0]):
     """
@@ -20,4 +21,18 @@ def radialAverage(image, center = [0,0]):
     -------
     [s, pattern] : list of ndarrays, shapes (M,)
     """
+    
+    #Preliminaries
+    x = n.asarray(range(image.shape[0]))
+    y = n.asarray(range(image.shape[1]))
+    
+    #Create meshgrid and compute radial positions of the data
+    X, Y = n.meshgrid(x,y)
+    R = n.sqrt( (X - center[0])**2 + (Y - center[1])**2 )
+    
+    #Flatten arrays
+    intensity = image.flatten()
+    radius = R.flatten()
+    
     return
+    #Sort by increasing radius
