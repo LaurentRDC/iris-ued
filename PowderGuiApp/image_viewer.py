70e5e9e210b4b3e3bcae75d7cf75c2af319d19b3
LaurentRDC <laurent.decotret@outlook.com>
2016-01-27 11:51:14 -0500

    Image fit to viewer

--- PowderGuiApp/image_viewer.py
+++ PowderGuiApp/image_viewer.py
@@ -10,9 +10,10 @@ class ImageViewer(pg.GraphicsLayoutWidget):
         super(ImageViewer, self).__init__()
         self.image = pg.ImageItem()
         self.curve = pg.PlotDataItem()
+        
         # A plot area (ViewBox + axes) for displaying the image
-        p1 = self.addPlot()
-        p1.addItem(self.image)
+        self.image_area = self.addPlot()
+        self.image_area.addItem(self.image)
         
         # Contrast/color control
         hist = pg.HistogramLUTItem()
@@ -20,28 +21,27 @@ class ImageViewer(pg.GraphicsLayoutWidget):
         self.addItem(hist)
         
         self.nextRow()
-        p2 = self.addPlot(colspan = 2)
-        p2.addItem(self.curve)
-        p2.setMaximumHeight(250)
+        self.curve_area = self.addPlot(colspan = 2)
+        self.curve_area.addItem(self.curve)
+        self.curve_area.setMaximumHeight(250)
         
         # Another plot area for displaying ROI data
         self.resize(800, 800)
-        self.show()
 
-        # zoom to fit imageo
-        p1.autoRange()  
+        # zoom to fit image
+        self.image_area.autoRange()
+        self.curve_area.autoRange()
     
-    def displayImage(self, image):
+    def displayImage(self, image, *args, **kwargs):
         if image is None:
             image = n.zeros(shape = (2048, 2048), dtype = n.float)
-        image = image.astype(n.float)
-        #TODO: Is downsampling a good idea?
-        self.image.setImage(image, autoDownsample = True)       
+            
+        self.image.setImage(image, *args, **kwargs)
+        self.image_area.autoRange() # zoom to fit image
     
-    def displayCurve(self, curve):
-        self.curve.setData(xValues = curve.xdata, yValues = curve.ydata)
-        
-## Start Qt event loop unless running in interactive mode or using pyside.
+    def displayCurve(self, curve, *args, **kwargs):
+        self.curve.setData(xValues = curve.xdata, yValues = curve.ydata, *args, **kwargs)
+
 if __name__ == '__main__':
     im = ImageViewer()
     im.show()
