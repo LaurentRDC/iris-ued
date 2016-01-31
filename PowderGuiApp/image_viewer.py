c76c689241d356823c8d07dbe1cde54c5b051d15
LaurentRDC <laurent.decotret@outlook.com>
2016-01-31 11:03:05 -0500

    Updated documentation

--- PowderGuiApp/image_viewer.py
+++ PowderGuiApp/image_viewer.py
@@ -6,10 +6,12 @@ import core
 pg.mkQApp()
 
 class ImageViewer(pg.GraphicsLayoutWidget):
+    """
+    Widget for displaying diffraction data. This widget has two sections:
+    an image area and a curve area.
     
-    #New-style signal definitiona
-    image_clicked = QtCore.pyqtSignal(tuple, name = 'image_clicked')
-    curve_clicked = QtCore.pyqtSignal(tuple, name = 'curve_clicked')
+    The ImageViewer can display an image and two curves at the same time.
+    """
     
     #Data transfer signals
     image_center_signal = QtCore.pyqtSignal(tuple, name = 'image_center_signal')
@@ -37,6 +39,7 @@ class ImageViewer(pg.GraphicsLayoutWidget):
         self.displayImage( image = None )
         
         #Signals
+        #TODO: a way to disable crosshair
         self.image_area.scene().sigMouseMoved.connect(self.updateCrosshair)
         
     def initUI(self):
@@ -89,10 +92,7 @@ class ImageViewer(pg.GraphicsLayoutWidget):
         self.mask.addScaleHandle([0, 0], [1, 1])
         
         self.center_finder = pg.CircleROI(pos = [1000,1000], size = [200,200], pen = pg.mkPen('r'))
-        
         self.cutoff_line = pg.InfiniteLine(angle = 90, movable = True, pen = pg.mkPen('r'))
-        
-        #TODO: determine the number of lines necessary
         self.inelasticBG_lines = [pg.InfiniteLine(angle = 90, movable = True, pen = pg.mkPen('b')) for i in range(6)]
     
     # -------------------------------------------------------------------------
@@ -141,6 +141,16 @@ class ImageViewer(pg.GraphicsLayoutWidget):
     # -------------------------------------------------------------------------
     
     def maskPosition(self):
+        """
+        Returns the x,y limits of the rectangular beam block mask. Due to the 
+        inversion of plotting axes, y-axis in the image_viewer is actually
+        the x-xis when analyzing data.
+        
+        Returns
+        -------
+        xmin, xmax, ymin, ymax : tuple
+            The limits determining the shape of the rectangular beamblock mask
+        """
         rect = self.mask.parentBounds().toRect()
         
         #If coordinate is negative, return 0
@@ -152,8 +162,15 @@ class ImageViewer(pg.GraphicsLayoutWidget):
         return y1, y2, x1, x2       #Flip output since image viewer plots transpose...
     
     def centerPosition(self, return_radius = False):
+        """
+        Returns
+        -------
+        x, y : tuple
+            center coordinates of the centerFinder object.
+        """
         corner_x, corner_y = self.center_finder.pos().x(), self.center_finder.pos().y()
         radius = self.center_finder.size().x()/2.0
+        
         #Flip output since image viewer plots transpose...
         if return_radius:
             return corner_y + radius, corner_x + radius, radius
@@ -161,17 +178,32 @@ class ImageViewer(pg.GraphicsLayoutWidget):
             return corner_y + radius, corner_x + radius
     
     def cutoffPosition(self):
+        """
+        Returns
+        -------
+        x,y : tuple
+            (x,y)-coordinate of the cutoff line with respect to the plotted data.
+            Only the x-value is meaningful.
+        """
         x = self.cutoff_line.value()
         return (x, 0)
     
     def inelasticBGPosition(self):
+        """
+        Returns
+        -------
+        intersects : list of tuples
+            list of (x,y) tuples representing the intersects of the inelastic BG fit
+            lines. Only the x-coordinates are meaningful.
+        """
         intersects = list()
         for line in self.inelasticBG_lines:
             x = line.value()
             intersects.append( (x, 0) )
         return intersects
     # -------------------------------------------------------------------------
-    #           RETURN DATA SLOTS AND SIGNALS
+    #           RETURN DATA SLOTS
+    #   These slots are used to emit data. 
     # -------------------------------------------------------------------------
     
     @QtCore.pyqtSlot()
@@ -198,18 +230,32 @@ class ImageViewer(pg.GraphicsLayoutWidget):
     #           PLOTTING METHODS
     # -------------------------------------------------------------------------
     
-    @QtCore.pyqtSlot(object, tuple, str)        
-    def displayImage(self, image, overlay = list(), overlay_color = 'r'):
+    @QtCore.pyqtSlot(object)        
+    def displayImage(self, image):
+        """ 
+        Display image in the form of a numpy array. 
+        
+        Parameters
+        ----------
+        image : numpy.ndarray
+        overlay : deprecated
+        overlay_color : deprecated
+        """
         if image is None:
             image = n.zeros(shape = (2048, 2048), dtype = n.float)
         self.image.setImage(image)
-        
-        #Add overlays
-        brush = pg.mkBrush(color = overlay_color)
-        self.image_overlay.setData(pos = overlay, size = 3, brush = brush)
     
     @QtCore.pyqtSlot(object)
     def displayRadialPattern(self, curve):
+        """ 
+        Displays one or two curves (overlayed) in to curve area.
+        
+        Parameters
+        ----------
+        curve : core.RadialCurve object or list of RadialCurve objects
+            Curves to be plotted. If 'curve' is a list, only the first two objects
+            will be plotted.
+        """
         #Distribute inputs
         if isinstance(curve, list):
             main_curve = curve[0]
