6d2ee5cadd87e30255ac123f1154322c42dcf3fc
LaurentRDC <laurent.decotret@outlook.com>
2016-01-31 21:58:24 -0500

    New settings modal dialog

--- PowderGuiApp/gui.py
+++ PowderGuiApp/gui.py
@@ -10,6 +10,7 @@ from core import *
 #GUI backends
 from pyqtgraph.Qt import QtCore, QtGui
 from image_viewer import ImageViewer
+from settings import SettingsDialog
 
 # -----------------------------------------------------------------------------
 #           COLOR PALETTE FOR PROGRESS
