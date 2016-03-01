adead9a7944d0306bddd036f9bb546b6051edd2c
LaurentRDC <laurent.decotret@outlook.com>
2016-03-01 14:57:32 -0500

    Included tifffile module

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from distutils.core import setup
+from setuptools import setup, find_packages
 import glob
 
 #To create a Windows installer for this, run:
@@ -10,13 +10,14 @@ image_list = glob.glob('PowderGuiApp\\images\\*.png')
 
 setup(
     name = 'PowderGui', 
-    version = 'v0.8', 
+    version = 'v0.9',
+    packages = find_packages(),
     description = 'UED Powder Diffraction Data Processing', 
     author = 'Laurent P. Rene de Cotret',
     url = 'www.physics.mcgill.ca/siwicklab',
     download_url = 'http://1drv.ms/1OVX2ac',
     scripts = ['post_installation_script.py'],
     py_modules = ['PowderGuiApp.core', 'PowderGuiApp.gui', 'PowderGuiApp.image_viewer'], 
-    install_requires = ['tqdm', 'numpy', 'pyqt4', 'pyqtgraph', 'scipy', 'tifffile', 'h5py'],
+    install_requires = ['tqdm', 'numpy', 'pyqt4', 'pyqtgraph', 'scipy', 'h5py'],
     data_files = [('PowderGuiApp\\images', image_list)]
     )
