b6f2df6ebd763892b5657a095448ab7efe37a1bc
LaurentRDC <laurent.decotret@outlook.com>
2016-04-12 10:00:35 -0400

    Release 1.4

---
+++ build/lib/iris/curve.py
@@ -0,0 +1,200 @@
+# -*- coding: utf-8 -*-
+
+#Basics
+import matplotlib.pyplot as plt
+import numpy as n
+from scipy.signal import find_peaks_cwt, ricker
+import scipy.optimize as opt
+
+def biexponential(x, amplitude1, amplitude2, decay1, decay2, offset1, offset2, floor):
+    """
+    Returns a biexponential function evaluated over an array.
+    
+    Notes
+    -----
+    In case of fitting, there are 7 free parameters, and thus at least 7 points
+    must be provided.
+    """
+    exp1 = amplitude1*n.exp(-decay1*(x - offset2))
+    exp2 = amplitude2*n.exp(-decay2*(x - offset1))
+    return exp1 + exp2 + floor
+        
+# -----------------------------------------------------------------------------
+#           RADIAL CURVE CLASS
+# -----------------------------------------------------------------------------
+
+class Curve(object):
+    """
+    This class represents any radially averaged diffraction pattern or fit.
+    
+    Attributes
+    ----------
+    xdata : ndarray, shape (N,)
+        Abscissa values of the curve.
+    ydata : ndarray, shape (N,)
+        Ordinate values of the curve.
+    name : str
+        String identifier.
+    color : str
+        Plotting color.
+    
+    Methods
+    -------    
+    inelastic_background
+        Fits a biexponential inelastic scattering background to the curve
+        
+    auto_inelastic_background
+        Fits a biexponential inelastic scattering background to the curve.
+        Background values are automatically determined from peak locations.
+    """
+    def __init__(self, xdata, ydata, name = '', color = 'b'):
+        
+        self.xdata = n.asarray(xdata, dtype = n.float)
+        self.ydata = n.asarray(ydata, dtype = n.float)
+        self.name = name
+        
+        #Plotting attributes
+        self.color = color
+    
+    def __sub__(self, pattern):
+        #Interpolate values so that substraction makes sense
+        return Curve(self.xdata, self.ydata - n.interp(self.xdata, pattern.xdata, pattern.ydata), name = self.name, color = self.color)
+    
+    def __add__(self, pattern):
+        #Interpolate values so that addition makes sense
+        return Curve(self.xdata, self.ydata + n.interp(self.xdata, pattern.xdata, pattern.ydata), name = self.name, color = self.color)
+    
+    def __copy__(self):
+        return Curve(self.xdata, self.ydata, self.name, self.color)
+    
+    def plot(self, show_peaks = True):
+        """ Diagnostic tool. """        
+        plt.figure()
+        plt.title(self.name)
+        plt.plot(self.xdata, self.ydata, self.color)
+        
+        if show_peaks:
+            for peak_index in self._find_peaks():
+                plt.axvline(self.xdata[peak_index], color = 'r')
+    
+    def inelastic_background(self, xpoints):
+        """
+        Better inelastic scattering background fit.
+        
+        Parameters
+        ----------
+        points : array-like, optional
+            x-values of the points to fit to.
+        """
+        # Preliminaries
+        xpoints = n.array(xpoints, dtype = n.float)
+        
+        def positivity_constraint(params):
+            """
+            This function returns a positive value if the background is less than
+            the data, everywhere.
+            """
+            return self.ydata - biexponential(self.xdata, *params)
+        
+        def residuals(params):
+            """
+            This function is 0 if the background passes through all required xpoints
+            """
+            background = biexponential(xpoints, *params)
+            ypoints = n.interp(xpoints, self.xdata, self.ydata)     # interpolation of the data at the background xpoints
+            return n.sum( (ypoints - background) ** 2 )
+            
+        constraints = {'type' : 'ineq', 'fun' : positivity_constraint}
+        
+        #Create initial guesses
+        # amp1, amp2, decay1, decay2, offset1, offset2, floor
+        # TODO: find better guesses?
+        guesses = (1,1,1,1,1,1,1)
+        
+        # Bounds detemined by logic and empiricism
+        bounds = [(0, 1e3*self.ydata.max()), (0, 1e3*self.ydata.max()), (-100, 100),(-100, 100), (-1e4, 1e4), (-1e4, 1e4), (-1e4, 1e4)]
+        
+        results = opt.minimize(residuals, x0 = guesses, bounds = bounds, method = 'SLSQP', constraints = constraints, options = {'disp' : True, 'maxiter' : 1000, 'ftol' : 1e-8})
+        
+        # Create inelastic background function 
+        amp1, amp2, dec1, dec2, off1, off2, floor = results.x
+        new_fit = biexponential(self.xdata, amp1, amp2, dec1, dec2, off1, off2, floor)
+        return Curve(self.xdata, new_fit, 'IBG {0}'.format(self.name), 'red')
+    
+    def auto_inelastic_background(self):
+        """
+        Fits the inelastic background by first finding the location of diffraction 
+        peaks, and automatically selecting points between peaks as background
+        intensities.
+            
+        Returns
+        -------
+        out : Curve object
+        """
+        # Find indices between peaks
+        xpoints = [self.xdata[int(i)] for i in self._between_peaks()]
+        return self.inelastic_background(xpoints)
+    
+    def _between_peaks(self):
+        """
+        Finds the indices associated with local minima between peaks in ydata
+        
+        Returns
+        -------
+        indices : list of ints
+            Indices of the throughs in ydata.
+        
+        Examples
+        --------
+        >>> import matplotlib.pyplot as plt
+        >>> plt.plot(curve.ydata)
+        >>> 
+        >>> indices = curve._find_peaks()
+        >>> for i in indices:
+        ...    plt.axvline(i)
+        ...
+        >>>
+        
+        See also
+        --------
+        scipy.signal.find_peaks_cwt
+        """
+        widths = n.arange(1, len(self.ydata)/10)    # Max width determined with testing
+        return find_peaks_cwt(-self.ydata, widths = widths, wavelet = ricker, 
+                              min_length = len(widths)/20, min_snr = 1.0)
+    
+    def _find_peaks(self):
+        """
+        Finds the indices associated with peaks in ydata
+        
+        Returns
+        -------
+        peak_indices : list of ints
+            Indices of the peaks in ydata.
+        
+        Examples
+        --------
+        >>> import matplotlib.pyplot as plt
+        >>> plt.plot(curve.ydata)
+        >>> 
+        >>> peak_indices = curve._find_peaks()
+        >>> for i in peak_indices:
+        ...    plt.axvline(i)
+        ...
+        >>>
+        
+        See also
+        --------
+        scipy.signal.find_peaks_cwt
+        """
+        widths = n.arange(1, len(self.ydata)/10)    # Max width determined with testing
+        return find_peaks_cwt(self.ydata, widths = widths, wavelet = ricker, 
+                              min_length = len(widths)/20, min_snr = 1.5)
+
+if __name__ == '__main__':
+    # Test curve
+    directory = 'K:\\2012.11.09.19.05.VO2.270uJ.50Hz.70nm'
+    from dataset import DiffractionDataset
+    d = DiffractionDataset(directory)
+    test = d.radial_pattern(0.0)
+    test.plot()
--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-from distutils.core import setup
-import py2exe
+from setuptools import setup
 import glob
 
 #To create a Windows installer for this, run:
@@ -10,14 +9,14 @@ import glob
 image_list = glob.glob('App\\images\\*.png')
 
 setup(
-    name = 'Iris - UED data exploration', 
-    version = 'v1.1',
-    packages = ['App.tifffile','App.pyqtgraph', 'App.tqdm'],
+    name = 'Iris', 
+    version = 'v1.4',
+    packages = ['iris.tifffile','iris.pyqtgraph'],
     description = 'UED data exploration', 
-    author = 'Laurent P. Rene de Cotret',
+    author = 'Laurent P. René de Cotret',
     url = 'www.physics.mcgill.ca/siwicklab',
     scripts = ['post_installation_script.py'],
-    py_modules = ['App.dataset', 'App.curve', 'App.iris'], 
+    py_modules = ['iris.dataset','iris.curve', 'iris.iris', 'iris.progress_widget'], 
     install_requires = ['numpy', 'pyqt4', 'scipy', 'h5py'],
     data_files = [('App\\images', image_list)]
     )
