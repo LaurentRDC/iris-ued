b264df3b515a551ff4f9b4d1b4c2b118727fdcb6
LaurentRDC <laurent.decotret@outlook.com>
2017-01-05 13:59:48 -0500

    FIX: removed build

--- build/lib/iris/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .dataset import DiffractionDataset, PowderDiffractionDataset
-from .optimizations import cached_property, parallel_map, parallel_sum
-from .utils import electron_wavelength, angular_average, scattering_length
-from .raw import RawDataset
