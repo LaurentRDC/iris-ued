ac71324182dba7d267457bf2c38369f85f67b0d3
LaurentRDC <laurent.decotret@outlook.com>
2017-01-13 12:32:09 -0500

    FIX: type casting
    Former-commit-id: 84a13ed899dfdda91d0b585276351216371923fa [formerly 84a13ed899dfdda91d0b585276351216371923fa [formerly d313b5fbf4bf9b737667be35a549b8fa846f88ea]]
    Former-commit-id: ff2ec745ccb11e6576df2483397c88a583198159
    Former-commit-id: 7e9611cc5b856002e3d720dce47614a89673021d

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ from setuptools import setup, find_packages
 import glob
 
 #To create a Windows installer for this, run:
-#
-# > python setup.py bdist_wininst
+# >>> python setup.py bdist_wininst
 
 image_list = glob.glob('iris\\gui\\images\\*.png')
 wavelets = chain.from_iterable([glob.glob('iris\\dualtree\\data\\*.npy'), 
@@ -15,13 +14,17 @@ rc = chain.from_iterable([glob.glob('iris\\gui\\qdarkstyle\\*.qrc'),
 
 setup(
     name = 'iris', 
-    version = '2.0.2',
+    version = '2.0.3',
     packages = find_packages(),
     description = 'UED data exploration', 
     author = 'Laurent P. René de Cotret',
     author_email = 'laurent.renedecotret@mail.mcgill.ca',
     url = 'www.physics.mcgill.ca/siwicklab',
-    install_requires = ['numpy', 'scipy', 'h5py', 'PyWavelets >= 0.5.1', 'tifffile', 'pyopengl'],    # PyOpenGL useful for PyQtgraph?
+    install_requires = ['numpy >= 1.11.2', 
+                        'scipy', 
+                        'h5py', 
+                        'PyWavelets >= 0.5.1', 
+                        'tifffile'],
     data_files = [('iris\\gui\\images', image_list),
                   ('iris\\dualtree\\data', wavelets),
                   ('iris\\gui\\qdarkstyle', rc)]
