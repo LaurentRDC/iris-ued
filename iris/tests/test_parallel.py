e57a31ba7728f8d8a78fb60d09eba23ca9d8f383
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-04-05 11:27:49 -0400

    Updated pmap to reduce to starmap for processes = 1
    No point in spinning up a multiprocessing.Pool if processes = 1

--- iris/tests/test_parallel.py
+++ iris/tests/test_parallel.py
@@ -14,10 +14,22 @@ class PMapTest(unittest.TestCase):
         result = pmap(identity, integers)
         self.assertEqual(integers, result)
     
+    def test_map_one_process(self):
+        """ Test pmap with a single process, which does not use multiprocessing.Pool.map """
+        integers = list(range(0,10))
+        result = pmap(identity, integers, processes = 1)
+        self.assertEqual(integers, list(result))
+    
     def test_trivial_map_with_args_and_kwargs(self):
         integers = list(range(0,10))
         result = pmap(identity, integers, args = (1,), kwargs = {'test' : True})
         self.assertEqual(result, integers)
+    
+    def test_on_generator(self):
+        """ Test pmap on an input generator """
+        integers = range(0, 10)
+        self.assertEqual(list(integers), list(pmap(identity, integers, processes = 2)))
+        self.assertEqual(list(integers), list(pmap(identity, integers, processes = 1)))
 
 if __name__ == '__main__':
     unittest.main()
