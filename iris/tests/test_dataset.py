a75bc0c102b91412e49afd8c7717c4ae84678c7e
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-04-10 13:39:20 -0400

    Tests for DiffractionDataset and derivatives

---
+++ iris/tests/test_dataset.py
@@ -0,0 +1,101 @@
+
+from ..dataset import DiffractionDataset, PowderDiffractionDataset
+import numpy as n
+from numpy.random import random
+import os.path
+import tempfile
+import unittest
+
+n.random.seed(23)
+
+def dummy_dataset(**kwargs):
+    """ Create a dummy DiffractionDataset, fills it with random data, and returns the filename. 
+    All keyword arguments are passed to the DiffractionDataset's experimental parameters. """
+    dataset_attributes = {'nscans': tuple(range(0, 5)),
+                          'time_points': tuple(range(0, 10)),
+                          'acquisition_date': '0.0.0.0',
+                          'fluence': 10.0,
+                          'current': 0.0,
+                          'exposure': 5,
+                          'energy': 90,
+                          'resolution': (128, 128),
+                          'center': (64,64),
+                          'beamblock_rect': (0,0,0,0),
+                          'sample_type': 'single_crystal',
+                          'time_zero_shift': 0}
+    dataset_attributes.update(kwargs)
+
+    name = os.path.join(tempfile.gettempdir(), 'test_dataset.hdf5')
+    with DiffractionDataset(name = name, mode = 'w') as dataset:
+
+        dataset.experimental_parameters_group.attrs.update(dataset_attributes)
+
+        # Fill dataset with random data
+        dataset.pumpoff_pictures_group.create_dataset('pumpoff_pictures',
+                                                      data = random(size = dataset.resolution + (len(dataset.nscans),)))
+        
+        block_shape = dataset.resolution + (len(dataset.time_points),)
+        dataset.processed_measurements_group.create_dataset('intensity', data = random(size = block_shape))
+        dataset.processed_measurements_group.create_dataset('error', data = random(size = block_shape))
+        dataset.processed_measurements_group.create_dataset('background_pumpon', data = random(size = dataset.resolution))
+        dataset.processed_measurements_group.create_dataset('background_pumpoff', data = random(size = dataset.resolution))
+
+    return name
+
+def dummy_powder_dataset(**kwargs):
+    """ Create a dummy PowderDiffractionDataset, fills it with random data, and returns the filename. 
+    All keyword arguments are passed to the PowderDiffractionDataset's experimental parameters. """
+
+    kwargs.update({'sample_type': 'powder'})
+
+    with PowderDiffractionDataset(name = dummy_dataset(**kwargs), mode = 'r+') as dataset:
+        dataset._compute_angular_averages()
+        filename = dataset.name
+    
+    return filename
+
+class TestDiffractionDataset(unittest.TestCase):
+
+    def test_corrected_time_points(self):
+        with DiffractionDataset(name = dummy_dataset(), mode = 'r+') as dataset:
+            dataset.time_zero_shift = 0
+            self.assertSequenceEqual(dataset.corrected_time_points, dataset.time_points)
+    
+    def test_averaged_data_retrieval(self):
+        """ Test the size of the output from DiffractionDataset.averaged_data 
+        and DiffractionDataset.averaged_error """
+        with DiffractionDataset(name = dummy_dataset(), mode = 'r+') as dataset:
+            full_shape = dataset.resolution + (len(dataset.time_points),)
+
+            full_data = dataset.averaged_data(timedelay = None)
+            full_error = dataset.averaged_error(timedelay = None)
+            self.assertSequenceEqual(full_data.shape, full_shape)
+            self.assertSequenceEqual(full_error.shape, full_shape)
+
+            time_slice = dataset.averaged_data(timedelay = dataset.time_points[0])
+            error_slice = dataset.averaged_error(timedelay = dataset.time_points[0])
+            self.assertSequenceEqual(time_slice.shape, dataset.resolution)
+            self.assertSequenceEqual(error_slice.shape, dataset.resolution)
+    
+    def test_averaged_data_retrieval_out_parameter(self):
+        """ Tests that the out parameter is working properly for  DiffractionDataset.averaged_data 
+        and DiffractionDataset.averaged_error """
+        with DiffractionDataset(name = dummy_dataset(), mode = 'r+') as dataset:
+            full_shape = dataset.resolution + (len(dataset.time_points),)
+            container = n.empty(full_shape)
+
+            dataset.averaged_data(timedelay = None, out = container)
+            self.assertTrue(n.allclose(container, dataset.averaged_data(None)))
+
+            dataset.averaged_error(timedelay = None, out = container)
+            self.assertTrue(n.allclose(container, dataset.averaged_error(None)))
+    
+    def test_averaged_data_retrieval_nonexisting_timepoint(self):
+        """ Tests that trying to access a non-existing time-point raises an appropriate error """
+        with DiffractionDataset(name = dummy_dataset(), mode = 'r+') as dataset:
+
+            with self.assertRaises(ValueError):
+                time_slice = dataset.averaged_data(timedelay = n.sum(dataset.time_points))
+            
+            with self.assertRaises(ValueError):
+                error_slice = dataset.averaged_error(timedelay = n.sum(dataset.time_points))
