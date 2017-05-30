7f8c0a2935a4ed4e3783d0ac45820dcff64c4226
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-05-30 12:59:47 -0400

    Electron coutn and beam properties function beam_properties()
    Former-commit-id: ce8bd1a0a15c08fe0b37de9da0d8483a21e8fc35 [formerly ce8bd1a0a15c08fe0b37de9da0d8483a21e8fc35 [formerly 67e8050c0ce555f76bc6a9d5b3a1c4cc039e83c8]]
    Former-commit-id: 8dbda9e95f9a8a411aee0c0f38105d0cdadca23f
    Former-commit-id: ed30d20e374bbe890f1d74ea562467667d17d3f1

---
+++ iris/gui/beam_properties_dialog.py
@@ -0,0 +1,77 @@
+
+import pyqtgraph as pg
+from pyqtgraph import QtCore, QtGui
+
+class ElectronBeamPropertiesDialog(QtGui.QDialog):
+    """
+    Modal dialog used to calculate electron count and other electron
+    beam properties
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.setModal(True)
+        self.setWindowTitle('Electron Beam Properties')
+
+        self.progress_bar = QtGui.QProgressBar(parent = self)
+
+        self.viewer = pg.ImageView(parent = self)
+        self.viewer.setImage(image)
+
+        self.mask = pg.ROI(pos = [800,800], size = [200,200], pen = pg.mkPen('r'))
+        self.mask.addScaleHandle([1, 1], [0, 0])
+        self.mask.addScaleHandle([0, 0], [1, 1])
+        self.viewer.getView().addItem(self.mask)
+
+        self.processes_widget = QtGui.QSpinBox(parent = self)
+        self.processes_widget.setRange(1, cpu_count() - 1)
+        self.processes_widget.setValue(min([cpu_count(), 7]))
+
+        self.save_btn = QtGui.QPushButton('Launch processing', self)
+        self.save_btn.clicked.connect(self.accept)
+
+        self.cancel_btn = QtGui.QPushButton('Cancel', self)
+        self.cancel_btn.clicked.connect(self.reject)
+        self.cancel_btn.setDefault(True)
+
+        # Determine settings
+        self.file_dialog = QtGui.QFileDialog(parent = self)
+
+        processes_layout = QtGui.QHBoxLayout()
+        processes_layout.addWidget(QtGui.QLabel('Number of cores to use:'))
+        processes_layout.addWidget(self.processes_widget)
+
+        buttons = QtGui.QHBoxLayout()
+        buttons.addWidget(self.save_btn)
+        buttons.addWidget(self.cancel_btn)
+
+        self.layout = QtGui.QVBoxLayout()
+        self.layout.addWidget(self.viewer)
+        self.layout.addLayout(processes_layout)
+        self.layout.addLayout(buttons)
+        self.setLayout(self.layout)
+    
+    @QtCore.pyqtSlot()
+    def accept(self):
+
+        # Beamblock rect
+        rect = self.mask.parentBounds().toRect()
+        #If coordinate is negative, return 0
+        x1 = round(max(0, rect.topLeft().x() ))
+        x2 = round(max(0, rect.x() + rect.width() ))
+        y1 = round(max(0, rect.topLeft().y() ))
+        y2 = round(max(0, rect.y() + rect.height() ))
+
+        beamblock_rect = (y1, y2, x1, x2)       #Flip output since image viewer plots transpose
+        filename = self.file_dialog.getSaveFileName(filter = '*.hdf5')[0]
+        if filename == '':
+            return
+        
+        # The arguments to the iris.processing.process function
+        # more arguments will be added by controller
+        kwargs = {'destination':filename, 
+                  'beamblock_rect': beamblock_rect,
+                  'processes': self.processes_widget.value()}
+        
+        self.processing_parameters_signal.emit(kwargs)
+        super().accept()
