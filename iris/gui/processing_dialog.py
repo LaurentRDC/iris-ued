303ed5d2855127e48676195af9b039c479065512
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-13 09:49:24 -0400

    Further improvements

--- iris/gui/processing_dialog.py
+++ iris/gui/processing_dialog.py
@@ -6,6 +6,56 @@ from pyqtgraph import QtCore, QtGui
 from ..processing import process
 from ..raw import McGillRawDataset
 
+fletcher32_help = """ Adds a checksum to each chunk to detect data corruption. 
+Attempts to read corrupted chunks will fail with an error. 
+No significant speed penalty """
+
+shuffle_help = """ Block-oriented compressors like GZIP or LZF work better
+when presented with runs of similar values. Enabling the
+shuffle filter rearranges the bytes in the chunk and may
+improve compression ratio. """
+
+alignment_help = """If checked, diffraction patterns will be aligned 
+using masked normalized cross-correlation. This can double the processing time. """
+
+class H5FileWidget(QtGui.QFrame):
+    """ Widget specifying all HDF5 file properties """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.compression_filter_widget = QtGui.QComboBox()
+        self.compression_filter_widget.addItems(['None', 'lzf', 'gzip'])
+        self.compression_filter_widget.setCurrentText('None')
+
+        self.fletcher32_widget = QtGui.QCheckBox('Enable Fletcher32 filter (?)', parent = self)
+        self.fletcher32_widget.setToolTip(fletcher32_help)
+
+        self.shuffle_filter_widget = QtGui.QCheckBox('Enable shuffle filter (?)', parent = self)
+        self.shuffle_filter_widget.setToolTip(shuffle_help)
+        self.shuffle_filter_widget.setChecked(True)
+
+        # Same style as control bar
+        self.setFrameShadow(QtGui.QFrame.Sunken)
+        self.setFrameShape(QtGui.QFrame.Panel)
+
+        layout = QtGui.QFormLayout()
+        layout.addRow('Compression filter: ', self.compression_filter_widget)
+        layout.addRow(self.fletcher32_widget)
+        layout.addRow(self.shuffle_filter_widget)
+
+        self.setLayout(layout)
+
+    def file_params(self):
+        """ Returns a dictionary with HDF5 file parameters """
+        compression = self.compression_filter_widget.value()
+        fletcher32 = self.fletcher32_widget.isChecked()
+        shuffle = self.shuffle_filter_widget.isChecked()
+
+        if compression == 'None':
+            return dict()
+
+        return {'compression': compression, 'fletcher32': fletcher32, 'shuffle': shuffle}
+
 class ProcessingDialog(QtGui.QDialog):
     """
     Modal dialog used to select dataset processing options.
@@ -21,6 +71,7 @@ class ProcessingDialog(QtGui.QDialog):
         super().__init__(**kwargs)
         self.setModal(True)
         self.setWindowTitle('Diffraction Dataset Processing')
+        self.h5_file_widget = H5FileWidget(parent = self)
 
         image = raw.raw_data(timedelay = raw.time_points[0], scan = raw.nscans[0]) - raw.pumpon_background
 
@@ -36,7 +87,8 @@ class ProcessingDialog(QtGui.QDialog):
         self.processes_widget.setRange(1, cpu_count() - 1)
         self.processes_widget.setValue(min([cpu_count(), 7]))
 
-        self.alignment_tf_widget = QtGui.QCheckBox('Perform alignment', parent = self)
+        self.alignment_tf_widget = QtGui.QCheckBox('Perform alignment (?)', parent = self)
+        self.alignment_tf_widget.setToolTip(alignment_help)
         self.alignment_tf_widget.setChecked(True)
 
         # Set exclude scan widget with a validator
@@ -56,16 +108,14 @@ class ProcessingDialog(QtGui.QDialog):
         # Determine settings
         self.file_dialog = QtGui.QFileDialog(parent = self)
 
-        processes_layout = QtGui.QFormLayout()
-        processes_layout.addRow('Number of cores to use:',self.processes_widget)
-
-        exclude_layout = QtGui.QFormLayout()
-        exclude_layout.addRow('Scans to exclude: ', self.exclude_scans_widget)
+        processing_options = QtGui.QFormLayout()
+        processing_options.addRow('Number of cores to use:',self.processes_widget)
+        processing_options.addRow('Scans to exclude: ', self.exclude_scans_widget)
+        processing_options.addRow(self.alignment_tf_widget)
 
         params_layout = QtGui.QHBoxLayout()
-        params_layout.addLayout(exclude_layout)
-        params_layout.addLayout(processes_layout)
-        params_layout.addWidget(self.alignment_tf_widget)
+        params_layout.addLayout(processing_options)
+        params_layout.addWidget(self.h5_file_widget)
 
         buttons = QtGui.QHBoxLayout()
         buttons.addWidget(self.save_btn)
@@ -92,6 +142,9 @@ class ProcessingDialog(QtGui.QDialog):
         filename = self.file_dialog.getSaveFileName(filter = '*.hdf5')[0]
         if filename == '':
             return
+
+        # HDF5 compression kwargs
+        ckwargs = self.h5_file_widget.file_params()
         
         exclude_scans_text = self.exclude_scans_widget.text()
         try:
@@ -109,7 +162,8 @@ class ProcessingDialog(QtGui.QDialog):
                   'beamblock_rect': beamblock_rect,
                   'processes': self.processes_widget.value(),
                   'exclude_scans': exclude_scans,
-                  'align': self.alignment_tf_widget.isChecked()}
+                  'align': self.alignment_tf_widget.isChecked(),
+                  'ckwargs': ckwargs}
         
         self.processing_parameters_signal.emit(kwargs)
         super().accept()
