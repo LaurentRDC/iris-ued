3f9e0f07a509faf8294d2ce278b89098246ed0ec
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-04-13 13:30:08 -0400

    Bumped to alpha 3
    Former-commit-id: 424b7252d0cdcbd72207a6638807e57da583b75a [formerly 424b7252d0cdcbd72207a6638807e57da583b75a [formerly cb1afd8799e2022f83929afe906224bd2bd5a9ae [formerly 6c19c9168e47d1f26701b967e0dbe3a91202513f]]]
    Former-commit-id: cb1afd8799e2022f83929afe906224bd2bd5a9ae
    Former-commit-id: 80f51d2424cad9f8e17446c4abf962382c3ebc89
    Former-commit-id: 84b9f97417fdab2cc6ac1a5993245feab41ae1b5
    Former-commit-id: 03b439709b5e70f1660e3af7625a792a2808d69e

--- iris/__init__.py
+++ iris/__init__.py
@@ -2,7 +2,7 @@
 __author__  = 'Laurent P. René de Cotret'
 __email__   = 'laurent.renedecotret@mail.mcgill.ca'
 __license__ = 'MIT'
-__version__ = '5.0a2'
+__version__ = '5.0a3'
 
 # Versioning should be compliant with PyPI guide 
 # https://packaging.python.org/tutorials/distributing-packages/#choosing-a-versioning-scheme
