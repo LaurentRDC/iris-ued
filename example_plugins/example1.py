bc1d9f64c90df919d68fd19558c8ac12f14baaee
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-04-13 14:28:04 -0400

    Added example plug-in
    Former-commit-id: 7d59f4a4018a053bf2a425e3d2d9dac131a7a9f0 [formerly 7d59f4a4018a053bf2a425e3d2d9dac131a7a9f0 [formerly baa0b21991e3ed384ca6354f30836ccbe822f5db [formerly 4d32e0b5be3f7fe9448840bd39e8d95444c68a8c]]]
    Former-commit-id: baa0b21991e3ed384ca6354f30836ccbe822f5db
    Former-commit-id: e803295a8b91298cab1b4ee5636bf9df0416b949
    Former-commit-id: ec1ca5d2d61591a32aa876589f80b332846e21ef
    Former-commit-id: f58cd48481358b75d2e365cbddb4a6bb9b717596

---
+++ example_plugins/example1.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Example Plugin
+==============
+"""
+
+from iris import AbstractRawDataset
+from iris.meta import ExperimentalParameter
+import numpy as np
+
+class MinimalRawDataset(AbstractRawDataset):
+    """
+    Minimal raw dataset interface. This is a showcase of what is possible with iris plug-ins.
+
+    This file should be placed as-is in the :file:`~\iris_plugins` folder 
+    (:file:`C:\\Users\\Username\\iris_plugins` on Windows)
+
+    Minimal full implementation is only the :meth:`raw_data` method.
+    """
+    # While the temperature metadata is already implemented in AbstractRawDataset,
+    # it can be overriden in subclasses
+    temperature = ExperimentalParameter('temperature', ptype = float, default = 500)
+
+    # New metadata that was not defined in AbstractRawDataset can also be added
+    is_useful = ExperimentalParameter('is_useful', ptype = bool, default = True)
+
+    def __init__(self, source = None, metadata = dict()):
+        # Metadata can be filled as a dictionary before
+        # initialization. # Attributes which are not 
+        # ExperimentalParameters are ignored.
+        metadata.update(
+            {'temperature'          : 100,
+             'exposure'             : 1,
+             'this_will_be_ignored' : True}
+        )
+        super().__init__(source, metadata)
+
+        # Metadata can also be changed attribute by attribute
+        # Attributes which are not ExperimentalParameters
+        self.time_points = tuple(range(0, 10))
+        self.resolution = (512, 512)
+        self.scans = tuple(range(1, 10))
+        self.notes = 'This is only an example'
+        self.is_useful = False
+
+    def raw_data(self, *args, **kwargs):
+        return np.random.random(size = (512, 512))
