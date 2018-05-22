FIX: azimuthal averages
Former-commit-id: 37f248c5b0a6060e599ac2f129f6e6789d88653e [formerly 37f248c5b0a6060e599ac2f129f6e6789d88653e [formerly 77896d118e3ea3161fed29dfc492c23ebe4c863e [formerly 3f9cdf6af939ce08de08eba0789f73442f7ea526]]]
Former-commit-id: 77896d118e3ea3161fed29dfc492c23ebe4c863e
Former-commit-id: 6ee1747122bdaa109ee038ae40fde4fcd185fef9
Former-commit-id: de34917b60d9dc605f7d7363c2b25532f6ea25fc
Former-commit-id: f2a6d2e232f06447189badd55da2a72dfcf5a052

iris/dataset.py
@@ -843,9 +843,11 @@ class PowderDiffractionDataset(DiffractionDataset):
         self.powder_group['intensity'].resize(rintensity.shape)
         self.powder_group['intensity'].write_direct(rintensity)
         
-        # We store the raw px radius and infer other measurements (e.g. diffraction angle) from it
         self.powder_group['px_radius'].resize(px_radius.shape)
         self.powder_group['px_radius'].write_direct(px_radius)
+
+        self.powder_group['scattering_vector'].resize(px_radius.shape)
+        self.powder_group['scattering_vector'].write_direct(px_radius)
         
         self.powder_group['baseline'].resize(rintensity.shape)
         self.powder_group['baseline'].write_direct(np.zeros_like(rintensity))