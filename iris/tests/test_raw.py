43af07c44a1a9148ceac0e91420fe6a0d524da7b
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-06-11 15:03:38 -0400

    Removed legacy dataset tests
    Former-commit-id: eb32e043f62e68be094d6b79a6d7bf18fd014295 [formerly eb32e043f62e68be094d6b79a6d7bf18fd014295 [formerly 711411157baeffde77c04fcca0977929b9dcbbc4]]
    Former-commit-id: 960230a352869753a519940d2518e4be777f65ff
    Former-commit-id: f6b03c003b8e0f29b8a82493b3e96a0346a3ffa7
    Former-commit-id: b15a2d54590627a7a7acb176b78a9fb68a0a3e68

--- iris/tests/test_raw.py
+++ iris/tests/test_raw.py
@@ -1,23 +1,12 @@
 # -*- coding: utf-8 -*-
-from .. import McGillRawDataset, AbstractRawDataset, check_raw_bounds
-from ..meta import ExperimentalParameter
-import numpy as np
 import unittest
 
-class TestRawDataset(AbstractRawDataset):
+import numpy as np
 
-    test        = ExperimentalParameter('test', int, default = 0)
-    resolution  = ExperimentalParameter('resolution', tuple, (16,16))
+from . import TestRawDataset
+from .. import AbstractRawDataset
+from ..meta import ExperimentalParameter
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.time_points = list(range(0, 10))
-        self.scans       = list(range(1, 3))
-
-    @check_raw_bounds
-    def raw_data(self, timedelay, scan = 1): 
-        return np.zeros((self.resolution), dtype = np.uint8)
 
 class TestAbstractRawDataset(unittest.TestCase):
 
