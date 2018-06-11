2068b3e7e5d73d9982107851ae0759dca15e863a
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-06-11 16:49:14 -0400

    Added diff_apply to operate on diffraction patterns one-by-one
    Former-commit-id: 041a98007c94be8275126b61400aced4bef6fa66 [formerly 041a98007c94be8275126b61400aced4bef6fa66 [formerly ab2047138dccbdae3d175202732971c4816ee70b]]
    Former-commit-id: f3ce61aea062591141a92dbdaf8f84bbbff3dcbc
    Former-commit-id: 6ae0d4715f565544ab94010ee55e4ead03c0c895
    Former-commit-id: 705b360c7394f4f27661783220810bf5cb264c54

--- iris/__init__.py
+++ iris/__init__.py
@@ -4,9 +4,6 @@ __email__   = 'laurent.renedecotret@mail.mcgill.ca'
 __license__ = 'MIT'
 __version__ = '5.0.3'
 
-# Versioning should be compliant with PyPI guide 
-# https://packaging.python.org/tutorials/distributing-packages/#choosing-a-versioning-scheme
-
 from .raw import AbstractRawDataset, check_raw_bounds
 from .mcgill import McGillRawDataset, LegacyMcGillRawDataset
 from .dataset import DiffractionDataset, PowderDiffractionDataset
