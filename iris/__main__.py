4b33ffb64c3d9a45bddd53f26fca618734dd8e72
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-06-13 09:07:24 -0400

    FIX: main
    Former-commit-id: feabdb1a0c313e0ec95e6cdf65592bfb8ef8756f [formerly feabdb1a0c313e0ec95e6cdf65592bfb8ef8756f [formerly f0f5b50e3c2c1afd4fbdf377148bb0ad11d53c7c]]
    Former-commit-id: 354f63136dbefd71aa4f1d5644755c4ee1c1604c
    Former-commit-id: 1c0146680e04d27dfda37e0ad8a7ab357a1f09d4
    Former-commit-id: a4db879258e09ded7737432a9d96c933254ef146

--- iris/__main__.py
+++ iris/__main__.py
@@ -5,5 +5,4 @@ import sys
 from iris.gui import run
 
 if __name__ == '__main__':
-    path = 'D:\\Diffraction data\\2018.03.05 - TiSe2 overnight 2\\symmetrized.hdf5'
-    sys.exit(run(path))
+    sys.exit(run())
