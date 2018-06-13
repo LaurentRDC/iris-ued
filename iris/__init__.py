a0088cb15bd1513ede74f94de3cef23b123661f6
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-06-13 08:50:27 -0400

    Added open_raw to open raw datasets by guessing the format
    Former-commit-id: d245189f6083ab5829d9e25c862ccad4b4075b07

--- iris/__init__.py
+++ iris/__init__.py
@@ -4,7 +4,7 @@ __email__   = 'laurent.renedecotret@mail.mcgill.ca'
 __license__ = 'MIT'
 __version__ = '5.0.3'
 
-from .raw import AbstractRawDataset, check_raw_bounds
+from .raw import AbstractRawDataset, check_raw_bounds, open_raw
 from .mcgill import McGillRawDataset, LegacyMcGillRawDataset
 from .dataset import DiffractionDataset, PowderDiffractionDataset
 from .meta import ExperimentalParameter
