80587090087e4fccde4765cc1de4676311e6e238
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-06-20 09:21:04 -0400

    Bump version
    Former-commit-id: 4c7b06331a50b97f51e44ca7fb729479ccbd2a21

--- iris/__init__.py
+++ iris/__init__.py
@@ -2,7 +2,7 @@
 __author__  = 'Laurent P. René de Cotret'
 __email__   = 'laurent.renedecotret@mail.mcgill.ca'
 __license__ = 'MIT'
-__version__ = '5.0.4'
+__version__ = '5.0.5'
 
 from .raw import AbstractRawDataset, check_raw_bounds, open_raw
 from .mcgill import McGillRawDataset, LegacyMcGillRawDataset
