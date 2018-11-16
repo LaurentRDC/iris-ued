fbe5cb9e42fd3daf6655275ff837b13f2c104ff8
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-11-16 13:24:56 -0500

    FIX: black formatting for setup.py
    Former-commit-id: 03693ffdf4a33b36819d3ed1f60229532643f2d8 [formerly 03693ffdf4a33b36819d3ed1f60229532643f2d8 [formerly 56714654668603356572f764681b30819ef21bb1]]
    Former-commit-id: ffa38102f359cf3bb2380f7a9b65ec73dd4e4234
    Former-commit-id: d21de7cfba74ab826e78427333ce37d62ad61aba
    Former-commit-id: f82124a4adca325f0bf52401ba440d344891a4a4

--- setup.py
+++ setup.py
@@ -20,8 +20,12 @@ BASE_PACKAGE    = 'iris'
 base_path = os.path.dirname(__file__)
 with open(os.path.join(base_path, BASE_PACKAGE, '__init__.py')) as f:
     module_content = f.read()
-    VERSION = re.compile(r'.*__version__ = \'(.*?)\'', re.S).match(module_content).group(1)
-    LICENSE = re.compile(r'.*__license__ = \'(.*?)\'', re.S).match(module_content).group(1)
+    VERSION = (
+        re.compile(r".*__version__ = \"(.*?)\"", re.S).match(module_content).group(1)
+    )
+    LICENSE = (
+        re.compile(r".*__license__ = \"(.*?)\"", re.S).match(module_content).group(1)
+    )
 
 with open('README.rst') as f:
     README = f.read()
