7c680cf6dc1349d85f0063d5c7429744483771c3
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-11-19 15:24:38 -0500

    Better command line interface
    Former-commit-id: 486c37fd61d61e213a3c2ba683e4ba8fc080c307 [formerly 486c37fd61d61e213a3c2ba683e4ba8fc080c307 [formerly 9ce756f71b3b465c8accd84a5f520097c1ad7e06]]
    Former-commit-id: c237aad5fe93499e5f0e8a1c986669acfb1d10b2
    Former-commit-id: d6f4ff007c20257ee51d517cf5fa4fed00571d97
    Former-commit-id: 8ba17053bfed0cf2022c60ddf39d38758030ee58

--- iris/__main__.py
+++ iris/__main__.py
@@ -2,41 +2,72 @@
 
 import argparse
 import sys
+import webbrowser
 from pathlib import Path
+
+from iris.gui import run
 
 from . import __version__
 from .pack import pack
 
-from iris.gui import run
-
 DESCRIPTION = """Iris is both a library for interacting with ultrafast electron 
-diffraction data, as well as a GUI frontend for interactively exploring this data."""
+diffraction data, as well as a GUI frontend for interactively exploring this data.
 
-EPILOG = """Documentation is available here: https://iris-ued.readthedocs.io/"""
+Below are some helpful commands. """
 
-PATH_HELP = """Raw or reduced dataset to open, if any. 
-The type of dataset will be guessed based on currently-installed plugins."""
+EPILOG = """Running this command without any parameters will 
+launch the graphical user interface."""
 
-PACK_HELP = """ Pack a raw dataset into a compressed HDF5 archive, resulting in space 
-savings of up to 4x. Archives can be loaded into iris."""
+OPEN_HELP = """Dataset to open with iris start-up. """
+
+PACK_HELP = """Pack a raw dataset into a compressed HDF5 archive, resulting in space 
+savings of up to 4x. Archives can be loaded into iris. """
+
+DOCS_HELP = """Open online documentation in your default web browser."""
 
 parser = argparse.ArgumentParser(prog="iris", description=DESCRIPTION, epilog=EPILOG)
 parser.add_argument("-v", "--version", action="version", version=f"iris {__version__}")
-parser.add_argument("path", help=PATH_HELP, type=Path, nargs="?", default=None)
 
-subparsers = parser.add_subparsers(help="Available sub-commands")
+subparsers = parser.add_subparsers(
+    title="Subcommands", help="Available sub-commands", dest="subcmd"
+)
+
+# Parser to open a path
+# To facilitate format determination, we need flags specifying whether the
+# path points to a raw, compact, or reduced dataset
+open_parser = subparsers.add_parser("open", help=OPEN_HELP)
+open_parser.add_argument("path", help="Path to the dataset", type=Path, nargs="?", default=None)
+dset_modes = open_parser.add_mutually_exclusive_group(required=True)
+dset_modes.add_argument(
+    "--compact",
+    action="store_true",
+    help="This flag indicates that the path should be considered an compact raw dataset",
+)
+dset_modes.add_argument(
+    "--raw",
+    action="store_true",
+    help="This flag indicates that the path should be considered a raw dataset. Raw dataset format will be inferred from the installed plugins",
+)
+dset_modes.add_argument(
+    "--reduced",
+    action="store_true",
+    help="This flag indicates that the path should be considered a reduced dataset.",
+)
 
 # Parser for pack command
 pack_parser = subparsers.add_parser("pack", help=PACK_HELP)
 pack_parser.add_argument("--src", type=Path, required=True)
 pack_parser.add_argument("--dst", type=Path, required=True)
 
+# Parser to reach documentation
+docs_parser = subparsers.add_parser("docs", help=DOCS_HELP)
+
 if __name__ == "__main__":
     args = parser.parse_args()
 
     # If source and destination paths are provided, this is because we
     # want to pack
-    if hasattr(args, "src") and hasattr(args, "dst"):
+    if args.subcmd == "pack":
         try:
             pack(args.src, args.dst)
         except RuntimeError as e:
@@ -46,5 +77,15 @@ if __name__ == "__main__":
             print(f"Dataset {args.src} has been successfully packed into {args.dst}")
             sys.exit(0)
 
-    # Otherwise, default behavior
-    sys.exit(run(path=args.path))
+    elif (args.subcmd == "open"):
+
+        # Otherwise, default behavior
+        sys.exit(run(path=args.path))
+    
+    elif args.subcmd == "docs":
+        webbrowser.open("https://iris-ued.readthedocs.io")
+        sys.exit(0)
+    
+    # Default behavior : open gui without loading any data
+    else:
+        sys.exit(run(path=None))
