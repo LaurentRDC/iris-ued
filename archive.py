9a1281ff9f5bc92ab81262325226064d983e6842
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-11-19 14:47:21 -0500

    Removed unused script
    Former-commit-id: b0d10bb4e91968a4811a82ce20f142dba973c8f2

--- archive.py
+++
@@ -1,8 +0,0 @@
-from iris import pack, open_raw
-
-if __name__ == "__main__":
-    with open_raw(
-        "D:\\Diffraction data\\SnSe\\SnSe Batch 3 Sample E3\\2018-11-16 - SnSe Batch 2 Sample E3"
-    ) as raw:
-        print(raw)
-        pack(raw, "C:\\Users\\Laurent\\Desktop\\archive.hdf5")
