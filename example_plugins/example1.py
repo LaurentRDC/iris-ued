2a8b0695bd956474b498b57be7fcd79608dd7a60
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-11-19 14:16:06 -0500

    Added ability to pack datasets into archives

--- example_plugins/example1.py
+++ example_plugins/example1.py
@@ -7,6 +7,7 @@ Example Plugin
 from iris import AbstractRawDataset, ExperimentalParameter
 import numpy as np
 
+
 class MinimalRawDataset(AbstractRawDataset):
     """
     Minimal raw dataset interface. This is a showcase of what is possible with iris plug-ins.
@@ -16,21 +17,20 @@ class MinimalRawDataset(AbstractRawDataset):
 
     Minimal full implementation is only the :meth:`raw_data` method.
     """
+
     # While the temperature metadata is already implemented in AbstractRawDataset,
     # it can be overriden in subclasses
-    temperature = ExperimentalParameter('temperature', ptype = float, default = 500)
+    temperature = ExperimentalParameter("temperature", ptype=float, default=500)
 
     # New metadata that was not defined in AbstractRawDataset can also be added
-    is_useful = ExperimentalParameter('is_useful', ptype = bool, default = True)
+    is_useful = ExperimentalParameter("is_useful", ptype=bool, default=True)
 
-    def __init__(self, source = None, metadata = dict()):
+    def __init__(self, source=None, metadata=dict()):
         # Metadata can be filled as a dictionary before
-        # initialization. # Attributes which are not 
+        # initialization. # Attributes which are not
         # ExperimentalParameters are ignored.
         metadata.update(
-            {'temperature'          : 100,
-             'exposure'             : 1,
-             'this_will_be_ignored' : True}
+            {"temperature": 100, "exposure": 1, "this_will_be_ignored": True}
         )
         super().__init__(source, metadata)
 
@@ -39,8 +39,8 @@ class MinimalRawDataset(AbstractRawDataset):
         self.time_points = tuple(range(0, 10))
         self.resolution = (512, 512)
         self.scans = tuple(range(1, 10))
-        self.notes = 'This is only an example'
+        self.notes = "This is only an example"
         self.is_useful = False
 
     def raw_data(self, *args, **kwargs):
-        return np.random.random(size = (512, 512))
+        return np.random.random(size=(512, 512))
