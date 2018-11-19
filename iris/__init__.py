41fc03b21120f3bd7605bd7f00e1267263f94826
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-11-19 14:16:06 -0500

    Added ability to pack datasets into archives
    Former-commit-id: 2a8b0695bd956474b498b57be7fcd79608dd7a60

--- iris/__init__.py
+++ iris/__init__.py
@@ -2,12 +2,13 @@
 __author__ = "Laurent P. René de Cotret"
 __email__ = "laurent.renedecotret@mail.mcgill.ca"
 __license__ = "MIT"
-__version__ = "5.0.6"
+__version__ = "5.1.0"
 
 from .raw import AbstractRawDataset, check_raw_bounds, open_raw
 from .mcgill import McGillRawDataset, LegacyMcGillRawDataset
 from .dataset import DiffractionDataset, PowderDiffractionDataset
 from .meta import ExperimentalParameter
 from .plugins import install_plugin
+from .pack import CompactRawDataset, pack
 
 from . import plugins
--- iris/__main__.py
+++ iris/__main__.py
@@ -5,6 +5,7 @@ import sys
 from pathlib import Path
 
 from . import __version__
+from .pack import pack
 
 from iris.gui import run
 
@@ -16,10 +17,34 @@ EPILOG = """Documentation is available here: https://iris-ued.readthedocs.io/"""
 PATH_HELP = """Raw or reduced dataset to open, if any. 
 The type of dataset will be guessed based on currently-installed plugins."""
 
+PACK_HELP = """ Pack a raw dataset into a compressed HDF5 archive, resulting in space 
+savings of up to 4x. Archives can be loaded into iris."""
+
 parser = argparse.ArgumentParser(prog="iris", description=DESCRIPTION, epilog=EPILOG)
 parser.add_argument("-v", "--version", action="version", version=f"iris {__version__}")
 parser.add_argument("path", help=PATH_HELP, type=Path, nargs="?", default=None)
 
+subparsers = parser.add_subparsers(help="Available sub-commands")
+
+# Parser for pack command
+pack_parser = subparsers.add_parser('pack', help=PACK_HELP)
+pack_parser.add_argument('--src', type=Path, required=True)
+pack_parser.add_argument('--dst', type=Path, required=True)
+
 if __name__ == "__main__":
     args = parser.parse_args()
+    
+    # If source and destination paths are provided, this is because we
+    # want to pack
+    if hasattr(args, 'src') and hasattr(args, 'dst'):
+        try:
+            pack(args.src, args.dst)
+        except RuntimeError as e:
+            print('[iris pack] the following fatal error occured: ', str(e))
+            sys.exit(1)
+        else:
+            print(f'Dataset {args.src} has been successfully packed into {args.dst}')
+            sys.exit(0)
+
+    # Otherwise, default behavior
     sys.exit(run(path=args.path))
