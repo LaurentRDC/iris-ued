13475645d363514853656173384635717a744a1b
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-11-19 14:16:06 -0500

    Added ability to pack datasets into archives
    Former-commit-id: c9cb744ff4a3d7f9ed8ca456dda56e23c6956134 [formerly c9cb744ff4a3d7f9ed8ca456dda56e23c6956134 [formerly 41fc03b21120f3bd7605bd7f00e1267263f94826 [formerly 2a8b0695bd956474b498b57be7fcd79608dd7a60]]]
    Former-commit-id: 41fc03b21120f3bd7605bd7f00e1267263f94826
    Former-commit-id: d7eebdf1c0f5b1844ec9d953975d033f85224120
    Former-commit-id: 518d70be8b60baf3311dabd6199ed4624294de97
    Former-commit-id: db1d788e3429aa3792171ed26b81df9e3f02c08f

---
+++ iris/pack.py
@@ -0,0 +1,200 @@
+# -*- coding: utf-8 -*-
+"""
+Pack a RawDataset instance int oa compressed HDF5 file.
+"""
+
+from pathlib import Path
+
+import h5py
+import numpy as np
+
+from .meta import HDF5ExperimentalParameter, MetaHDF5Dataset, MetaRawDataset
+from .raw import AbstractRawDataset, open_raw
+
+
+def pack(source_fname, packed_fname):
+    """
+    Pack a raw dataset into a compressed HDF5 archive.
+
+    Parameters
+    ----------
+    source_fname : path-like
+        Path to the original RawDataset instance. Its type will be inferred
+        based on the available plugins, as determined by ``open_raw``.
+    packed_fname : path-like
+        Location of the resulting packed dataset.
+    """
+    try:
+        with open_raw(source_fname) as raw_dset:
+            CompactRawDataset.pack(raw_dset, packed_fname)
+    except RuntimeError:
+        raise RuntimeError('The source format could not be inferred.')
+
+
+class CompactMeta(MetaHDF5Dataset, MetaRawDataset):
+    """ Metaclass combining the features of HDF5 datasets with abstract raw dataset."""
+
+    def __init__(cls, clsname, bases, clsdict):
+        # We only want to keep the HDF5 experimental parameters
+        # Therefore, we initialize MetaRawDataset, then empty its valid_metadata
+        # attribute, which will be re-created by MetaHDF5Dataset
+        MetaRawDataset.__init__(cls, clsname, bases, clsdict)
+        cls.valid_metadata = set([])
+
+        MetaHDF5Dataset.__init__(cls, clsname, bases, clsdict)
+
+
+class CompactRawDataset(h5py.File, AbstractRawDataset, metaclass=CompactMeta):
+    """
+    This class represents a compressed RawDataset.
+
+    Parameters
+    ----------
+    source : path-like
+        Path to the HDF5 file.
+    """
+
+    # List of valid metadata below
+    # Using the HDF5ExperimentalParameter allows for automatic registering
+    # of the parameters as valid.
+    # These attributes can be accessed using the usual property access
+    date = HDF5ExperimentalParameter("date", str, default="")
+    energy = HDF5ExperimentalParameter("energy", float, default=90)  # keV
+    pump_wavelength = HDF5ExperimentalParameter(
+        "pump_wavelength", int, default=800
+    )  # nanometers
+    fluence = HDF5ExperimentalParameter("fluence", float, default=0)  # mj / cm**2
+    time_zero_shift = HDF5ExperimentalParameter(
+        "time_zero_shift", float, default=0
+    )  # picoseconds
+    temperature = HDF5ExperimentalParameter(
+        "temperature", float, default=293
+    )  # Kelvins
+    exposure = HDF5ExperimentalParameter("exposure", float, default=1)  # seconds
+    resolution = HDF5ExperimentalParameter("resolution", tuple, default=(2048, 2048))
+    time_points = HDF5ExperimentalParameter(
+        "time_points", tuple, default=tuple()
+    )  # picoseconds
+    scans = HDF5ExperimentalParameter("scans", tuple, default=(1,))
+    camera_length = HDF5ExperimentalParameter(
+        "camera_length", float, default=0.23
+    )  # meters
+    pixel_width = HDF5ExperimentalParameter(
+        "pixel_width", float, default=14e-6
+    )  # meters
+    notes = HDF5ExperimentalParameter("notes", str, default="")
+
+    def __init__(self, source, *args, **kwargs):
+        # Default mode is 'r'. We can't put this in the parameters
+        # because the class must follow the AbstractRawDataset __init__
+        mode = kwargs.pop("mode", "r")
+        super().__init__(name=source, mode=mode, **kwargs)
+
+    @property
+    def experimental_parameters_group(self):
+        return self.require_group(name="/")
+
+    @classmethod
+    def pack(cls, source, path, **kwargs):
+        """ 
+        Create a compact raw dataset from some other raw dataset.
+        
+        Parameters
+        ----------
+        source : AbstractRawDataset 
+            Any instance of ``AbstractRawDataset``.
+        fname : path-like
+            Path to the resulting HDF5 file.
+        """
+        if isinstance(path, Path):
+            path = str(path)
+
+        print(repr(source))
+
+        metadata = source.metadata
+        timedelays = metadata["time_points"]
+        scans = source.scans
+        scan_shape = tuple(source.resolution) + (len(timedelays),)
+        dtype = source.raw_data(timedelays[0], scans[0]).dtype
+
+        # During testing, it was found that higher compression ratio for gzip
+        # did not result in smaller files, but very long compression time.
+        dset_kwargs = {
+            "maxshape": scan_shape,
+            "chunks": True,
+            "shuffle": True,
+            "fletcher32": True,
+            "compression": "gzip",
+            "compression_opts": 4,
+        }
+
+        # Creating a placeholder for a full scan accelerates encoding
+        # Writing large blocks to file is much faster
+        scan_buffer = np.empty(shape=scan_shape, dtype=dtype, order="C")
+
+        # By default, fail to create if file exists
+        # For tests, however, better to force overwrite
+        if "mode" not in kwargs:
+            kwargs["mode"] = "w-"
+
+        with cls(path, **kwargs) as archive:
+
+            # Saving of metadata is provided by MetaHDF5Dataset
+            # Note that keys not associated with an HDF5ExperimentalParameter
+            # descriptor will not be recorded in the file.
+            for key, val in metadata.items():
+                if key not in cls.valid_metadata:
+                    continue
+                setattr(archive, key, val)
+
+            # Each scan is saved in its own dataset
+            gp = archive.create_group("Scans")
+
+            for scan in scans:
+
+                for index, image in enumerate(source.iterscan(scan)):
+                    scan_buffer[:, :, index] = image
+
+                # TODO: affix time stamps as an attribute on this dataset
+                dset = gp.create_dataset(
+                    "Scan {:d}".format(scan), data=scan_buffer, **dset_kwargs
+                )
+
+        return path
+
+    def raw_data(self, timedelay, scan=1, **kwargs):
+        """
+        Returns an array of the image at a timedelay and scan.
+        
+        Parameters
+        ----------
+        timdelay : float
+            Acquisition time-delay.
+        scan : int, optional
+            Scan number. Default is 1.
+        kwargs
+            Keyword-arguments are ignored.
+        
+        Returns
+        -------
+        arr : `~numpy.ndarray`, ndim 2
+        
+        Raises
+        ------
+        ValueError : if ``timedelay`` or ``scan`` are invalid / out of bounds.
+        IOError : Filename is not associated with an image/does not exist.
+        """
+        timedelay, scan = float(timedelay), int(scan)
+
+        # timedelay_index cannot be cast to int() if np.argwhere returns an empty array
+        # catch the corresponding TypeError
+        try:
+            timedelay_index = int(
+                np.argwhere(np.asarray(self.time_points) == timedelay)
+            )
+        except TypeError:
+            raise ValueError("Invalid time-delay")
+
+        dset = self["Scans/Scan {:d}".format(scan)]
+
+        return dset[:, :, timedelay_index]
--- setup.py
+++ setup.py
@@ -9,16 +9,16 @@ from setuptools import setup, find_packages
 #   >>> python setup.py sdist
 #   >>> twine upload dist/iris-x.x.x.tar.gz
 
-PACKAGE_NAME    = 'iris-ued'
-DESCRIPTION     = 'Ultrafast electron diffraction data exploration'
-URL             = 'http://iris-ued.readthedocs.io'
-DOWNLOAD_URL    = 'http://github.com/LaurentRDC/iris-ued'
-AUTHOR          = 'Laurent P. René de Cotret'
-AUTHOR_EMAIL    = 'laurent.renedecotret@mail.mcgill.ca'
-BASE_PACKAGE    = 'iris'
+PACKAGE_NAME = "iris-ued"
+DESCRIPTION = "Ultrafast electron diffraction data exploration"
+URL = "http://iris-ued.readthedocs.io"
+DOWNLOAD_URL = "http://github.com/LaurentRDC/iris-ued"
+AUTHOR = "Laurent P. René de Cotret"
+AUTHOR_EMAIL = "laurent.renedecotret@mail.mcgill.ca"
+BASE_PACKAGE = "iris"
 
 base_path = os.path.dirname(__file__)
-with open(os.path.join(base_path, BASE_PACKAGE, '__init__.py')) as f:
+with open(os.path.join(base_path, BASE_PACKAGE, "__init__.py")) as f:
     module_content = f.read()
     VERSION = (
         re.compile(r".*__version__ = \"(.*?)\"", re.S).match(module_content).group(1)
@@ -27,54 +27,59 @@ with open(os.path.join(base_path, BASE_PACKAGE, '__init__.py')) as f:
         re.compile(r".*__license__ = \"(.*?)\"", re.S).match(module_content).group(1)
     )
 
-with open('README.rst') as f:
+with open("README.rst") as f:
     README = f.read()
 
-with open('requirements.txt') as f:
-    REQUIREMENTS = [line for line in f.read().split('\n') if len(line.strip())]
+with open("requirements.txt") as f:
+    REQUIREMENTS = [line for line in f.read().split("\n") if len(line.strip())]
 
-exclude = {'exclude': ['docs', '*cache', '*tests']}
-PACKAGES = [BASE_PACKAGE + '.' + x for x in find_packages(os.path.join(base_path, BASE_PACKAGE), **exclude)]
+exclude = {"exclude": ["docs", "*cache", "*tests"]}
+PACKAGES = [
+    BASE_PACKAGE + "." + x
+    for x in find_packages(os.path.join(base_path, BASE_PACKAGE), **exclude)
+]
 if BASE_PACKAGE not in PACKAGES:
     PACKAGES.append(BASE_PACKAGE)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup(
-        name = PACKAGE_NAME,
-        description = DESCRIPTION,
-        long_description = README,
-        long_description_content_type = 'text/x-rst',
-        license = LICENSE,
-        url = URL,
-        download_url = DOWNLOAD_URL,
-        version = VERSION,
-        author = AUTHOR,
-        author_email = AUTHOR_EMAIL,
-        maintainer = AUTHOR,
-        maintainer_email = AUTHOR_EMAIL,
-        install_requires = REQUIREMENTS,
-        keywords = ['ultrafast electron diffraction visualization pyqtgraph'],
-        packages = PACKAGES,
-        data_files = [('iris\\gui\\images', glob('iris\\gui\\images\\*.png'))],
-        entry_points = {'gui_scripts': ['iris = iris.gui:run']},
-        include_package_data = True,
-        project_urls = {
-            'Documentation' : 'http://iris-ued.readthedocs.io/en/master/',
-            'Source'        : 'https://github.com/LaurentRDC/iris-ued',
-            'Tracker'       : 'https://github.com/LaurentRDC/iris-ued/issues',
-            'Home'          : 'http://www.physics.mcgill.ca/siwicklab/software.html'
+        name=PACKAGE_NAME,
+        description=DESCRIPTION,
+        long_description=README,
+        long_description_content_type="text/x-rst",
+        license=LICENSE,
+        url=URL,
+        download_url=DOWNLOAD_URL,
+        version=VERSION,
+        author=AUTHOR,
+        author_email=AUTHOR_EMAIL,
+        maintainer=AUTHOR,
+        maintainer_email=AUTHOR_EMAIL,
+        install_requires=REQUIREMENTS,
+        keywords=["ultrafast electron diffraction visualization pyqtgraph"],
+        packages=PACKAGES,
+        data_files=[("iris\\gui\\images", glob("iris\\gui\\images\\*.png"))],
+        entry_points={"gui_scripts": ["iris = iris.gui:run"]},
+        include_package_data=True,
+        project_urls={
+            "Documentation": "http://iris-ued.readthedocs.io/en/master/",
+            "Source": "https://github.com/LaurentRDC/iris-ued",
+            "Tracker": "https://github.com/LaurentRDC/iris-ued/issues",
+            "Home": "http://www.physics.mcgill.ca/siwicklab/software.html",
         },
-        python_requires = '>= 3.6',
-        classifiers = ['Development Status :: 5 - Production/Stable',
-                       'Intended Audience :: Science/Research',
-                       'Topic :: Scientific/Engineering',
-                       'Topic :: Scientific/Engineering :: Physics',
-                       'Topic :: Scientific/Engineering :: Chemistry',
-                       'Topic :: Scientific/Engineering :: Visualization',
-                       'License :: OSI Approved :: MIT License',
-                       'Natural Language :: English',
-                       'Operating System :: OS Independent',
-                       'Programming Language :: Python',
-                       'Programming Language :: Python :: 3.6',
-                       'Programming Language :: Python :: Implementation :: CPython']
-            )
+        python_requires=">= 3.6",
+        classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            "Intended Audience :: Science/Research",
+            "Topic :: Scientific/Engineering",
+            "Topic :: Scientific/Engineering :: Physics",
+            "Topic :: Scientific/Engineering :: Chemistry",
+            "Topic :: Scientific/Engineering :: Visualization",
+            "License :: OSI Approved :: MIT License",
+            "Natural Language :: English",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: Implementation :: CPython",
+        ],
+    )
