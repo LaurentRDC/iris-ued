3c42a01ad4c4f4cb0b89e5072857596a65981574
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-11-19 14:46:57 -0500

    Fixes and added progress using tqdm
    Former-commit-id: f20b35fb5ad3b48a3c1b5e10e9831c55d414c817 [formerly f20b35fb5ad3b48a3c1b5e10e9831c55d414c817 [formerly 4fd6988e03eaf0f0977ccb897c0009ecd555904c]]
    Former-commit-id: 70d5fe7134f3a6b7041fecfd9fb959a97c0f4472
    Former-commit-id: a723062109fb61f944040b85c51b269aca093fae
    Former-commit-id: 8fde4d1dfdd824c52312e5fc081f4874dcceb00f

--- iris/__main__.py
+++ iris/__main__.py
@@ -27,23 +27,23 @@ parser.add_argument("path", help=PATH_HELP, type=Path, nargs="?", default=None)
 subparsers = parser.add_subparsers(help="Available sub-commands")
 
 # Parser for pack command
-pack_parser = subparsers.add_parser('pack', help=PACK_HELP)
-pack_parser.add_argument('--src', type=Path, required=True)
-pack_parser.add_argument('--dst', type=Path, required=True)
+pack_parser = subparsers.add_parser("pack", help=PACK_HELP)
+pack_parser.add_argument("--src", type=Path, required=True)
+pack_parser.add_argument("--dst", type=Path, required=True)
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    
+
     # If source and destination paths are provided, this is because we
     # want to pack
-    if hasattr(args, 'src') and hasattr(args, 'dst'):
+    if hasattr(args, "src") and hasattr(args, "dst"):
         try:
             pack(args.src, args.dst)
         except RuntimeError as e:
-            print('[iris pack] the following fatal error occured: ', str(e))
+            print("[iris pack] the following fatal error occured: ", str(e))
             sys.exit(1)
         else:
-            print(f'Dataset {args.src} has been successfully packed into {args.dst}')
+            print(f"Dataset {args.src} has been successfully packed into {args.dst}")
             sys.exit(0)
 
     # Otherwise, default behavior
