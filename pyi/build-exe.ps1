c3ede64ec957fa74716d69dc2a5daf2e6a1c2837
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2019-03-11 16:19:32 -0400

    Added support for InnoSetup
    Former-commit-id: 0208993072b458734648e5fc352b79bf84ec4e8d [formerly 0208993072b458734648e5fc352b79bf84ec4e8d [formerly 623deebc1621cf0ee5eae028ee206df103127834 [formerly 0c9593fd1ff9eb7a2d962e6e22abc7f37204fb9f]]]
    Former-commit-id: 623deebc1621cf0ee5eae028ee206df103127834
    Former-commit-id: ce4e5b4732bfef97e15ca0ccfcd2f10f590a45d9
    Former-commit-id: ddc9e53911d986d3329687a4c19a65e6579fac6a
    Former-commit-id: 58e7f7d89014874721b8f663a13ba6247d46a161

--- pyi/build-exe.ps1
+++ pyi/build-exe.ps1
@@ -1,2 +1,11 @@
 "Building .exe pyinstaller"
 pyinstaller --clean --distpath=dist iris.exe.spec
+
+"Building setup using ISCC"
+if ($ENV:PROCESSOR_ARCHITECTURE -eq "AMD64"){
+    $iscc = get-item "C:\Program Files (x86)\Inno Setup 5\ISCC.exe"
+}
+else {
+    $iscc = get-item "C:\Program Files\Inno Setup 5\ISCC.exe"
+}
+& $iscc setup_axis_scip.iss #*> iscc.out
