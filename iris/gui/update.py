d8eb5908985ac0feb4a3b754df216b152a21637e
Laurent P. René de Cotret <LaurentRDC@users.noreply.github.com>
2019-03-11 20:11:29 -0400

    Removed auto-update mechanism

--- iris/gui/update.py
+++ iris/gui/update.py
@@ -6,22 +6,13 @@ Update checks management
 Based on the ``outdated`` package
 """
 import json
-from subprocess import Popen, run, PIPE
 from urllib.error import URLError
 from urllib.request import urlopen
 
+from PyQt5 import QtCore
 from pkg_resources import parse_version
 
 from .. import __version__
-
-try:
-    from subprocess import CREATE_NEW_PROCESS_GROUP
-
-    WINDOWS = True
-except ImportError:
-    WINDOWS = False
-
-DETACHED_PROCESS = 0x00000008  # 0x8 | 0x200 == 0x208
 
 
 def update_available():
@@ -51,29 +42,20 @@ def update_available():
     is_outdated = latest_version > parse_version(__version__)
     return is_outdated, str(latest_version)
 
+class UpdateChecker(QtCore.QThread):
+    """
+    Worker that checks for iris-ued updates in a separate thread. Using a separate QThread
+    prevents long start times.
+    """
+    update_available_signal = QtCore.pyqtSignal(bool)
 
-def update_in_background():
-    """ Update iris in the background. If iris-ued was installed with conda, it will be updated through conda as well;
-    otherwise, pip is used. """
-    # Determine if conda is installed
-    try:
-        conda_installed = run(["conda", "--version"]).exitcode == 0
-    except:
-        conda_installed = False
-
-    # Determine if iris-ued was installed with conda
-    # If so, we update it with conda
-    if conda_installed:
-        conda_list = json.loads(
-            run(["conda", "list", "--json", "--no-pip"], stdout=PIPE).stdout
-        )
-        update_with_conda = "iris-ued" in {item["name"] for item in conda_list}
-
-    flags = DETACHED_PROCESS
-    if WINDOWS:
-        flags = flags | CREATE_NEW_PROCESS_GROUP
-
-    if update_with_conda:
-        Popen(["conda", "update", "iris-ued", "--yes"], creationflags=flags)
-    else:
-        Popen(["pip", "install", "--upgrade", "iris-ued", "-y"], creationflags=flags)
+    def __init__(self, *args, **kwargs):
+        QtCore.QThread.__init__(self)
+    
+    def run(self):
+        try:
+            outdated, _ = update_available()
+        except ConnectionError:
+            outdated = False
+        
+        self.update_available_signal.emit(outdated)
