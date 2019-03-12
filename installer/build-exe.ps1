e7e668467e9f7e7a0e3be6645e8cf61422b1c24e
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2019-03-12 13:39:45 -0400

    Fixed an issue where frozen executable would crash when using multiprocessing

--- installer/build-exe.ps1
+++ installer/build-exe.ps1
@@ -1,7 +1,10 @@
-"Building .exe pyinstaller"
-python -OO -m PyInstaller --clean --distpath=dist iris.exe.spec
+Write-Host "Building .exe pyinstaller"
+$INSTALLATION_RESULT = python -O -m PyInstaller --clean --distpath=dist iris.exe.spec
+if($INSTALLATION_RESULT -ne 0){
+    Write-Host "PyInstaller run failed"
+}
 
-"Building setup using ISCC"
+Write-Host "Building setup using ISCC"
 if ($ENV:PROCESSOR_ARCHITECTURE -eq "AMD64"){
     $iscc = get-item "C:\Program Files (x86)\Inno Setup 5\ISCC.exe"
 }
