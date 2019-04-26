0eba61eaadad9f4780299ee91c4e4b1a4557b333
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2019-04-26 13:07:33 -0400

    Added preliminary support for logs
    Former-commit-id: 367269540a0b149290738b348cd72139cf2b6e7a [formerly 8bdd5b5e5e0eed9d83c1295943fa8ebd90b3e07e]
    Former-commit-id: 367269540a0b149290738b348cd72139cf2b6e7a
    Former-commit-id: 0bbacb5ee4dd01614697169a8a88e915de4edef5
    Former-commit-id: 0916ed81b71e8fe8012770be495e0c9a8dd361da
    Former-commit-id: a1abfa722be1fc431ab36a37cf264bfc77fe81ae

--- iris/gui/qdarkstyle/pyqt5_style_rc.py
+++ iris/gui/qdarkstyle/pyqt5_style_rc.py
@@ -1747,7 +1747,7 @@ qt_resource_struct_v2 = b"\
 \x00\x00\x01\x6a\x4f\xe2\xe5\x27\
 "
 
-qt_version = [int(v) for v in QtCore.qVersion().split('.')]
+qt_version = [int(v) for v in QtCore.qVersion().split(".")]
 if qt_version < [5, 8, 0]:
     rcc_version = 1
     qt_resource_struct = qt_resource_struct_v1
@@ -1755,10 +1755,17 @@ else:
     rcc_version = 2
     qt_resource_struct = qt_resource_struct_v2
 
+
 def qInitResources():
-    QtCore.qRegisterResourceData(rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data)
+    QtCore.qRegisterResourceData(
+        rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data
+    )
+
 
 def qCleanupResources():
-    QtCore.qUnregisterResourceData(rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data)
+    QtCore.qUnregisterResourceData(
+        rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data
+    )
+
 
 qInitResources()
