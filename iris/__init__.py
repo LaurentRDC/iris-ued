e4e256a6710a5552abd345a175f984dc7105cd5c
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2019-08-09 14:56:04 -0400

    Changed change definition of masks for alignment
    Former-commit-id: 1ae0e4b264bdcdb64673d4cc3dc527bfaa4ae266

--- iris/__init__.py
+++ iris/__init__.py
@@ -2,7 +2,7 @@
 __author__ = "Laurent P. René de Cotret"
 __email__ = "laurent.renedecotret@mail.mcgill.ca"
 __license__ = "MIT"
-__version__ = "5.1.4"
+__version__ = "5.2.0"
 
 from .raw import AbstractRawDataset, check_raw_bounds, open_raw
 from .mcgill import McGillRawDataset, LegacyMcGillRawDataset
