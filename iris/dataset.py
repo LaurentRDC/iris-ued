dcf75bfaac0243986217ac1c63a8389039ccd727
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2019-08-13 15:22:03 -0400

    Disable parallel execution for Unix-like
    Former-commit-id: e23d6451f08bfc306f6f3d1f92e7718a9a20b926

--- iris/dataset.py
+++ iris/dataset.py
@@ -21,7 +21,10 @@ from .meta import HDF5ExperimentalParameter, MetaHDF5Dataset
 
 # Whether or not single-writer multiple-reader (SWMR) mode is available
 # See http://docs.h5py.org/en/latest/swmr.html for more information
-SWMR_AVAILABLE = (h5py.version.hdf5_version_tuple > (1, 10, 0))
+SWMR_AVAILABLE = (h5py.version.hdf5_version_tuple > (1, 10, 0)) and (
+    platform == "win32"
+)
+
 
 class DiffractionDataset(h5py.File, metaclass=MetaHDF5Dataset):
     """
