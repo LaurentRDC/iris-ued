0ee13886a155d37fa8b8feda704fe09d0d88ebb1
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2019-09-18 13:29:25 -0400

    Removed McGill datasets (moved to iris-ued-plugins)
    Former-commit-id: fcd3c78bd3f2569979a7bf5f1d3d13d9cdbbb338

--- iris/__init__.py
+++ iris/__init__.py
@@ -5,7 +5,6 @@ __license__ = "MIT"
 __version__ = "5.2.0"
 
 from .raw import AbstractRawDataset, check_raw_bounds, open_raw
-from .mcgill import McGillRawDataset, LegacyMcGillRawDataset
 from .dataset import DiffractionDataset, PowderDiffractionDataset
 from .meta import ExperimentalParameter
 from .plugins import install_plugin
