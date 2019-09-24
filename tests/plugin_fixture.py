d31b406ed0482d1a16cbec9a3c13f480d3e84142
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2019-09-24 15:07:53 -0400

    Added test for plug-ins
    Former-commit-id: f4be5146ee50dd851010e7ef8c07be4e0bb1813c

---
+++ tests/plugin_fixture.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Text plug-in
+============
+"""
+
+from iris import AbstractRawDataset, ExperimentalParameter
+import numpy as np
+
+
+class TestRawDatasetPlugin(AbstractRawDataset):
+    
+    display_name = "Minimal raw dataset v1"
+
+    temperature = ExperimentalParameter("temperature", ptype=float, default=500)
+    is_useful = ExperimentalParameter("is_useful", ptype=bool, default=True)
+
+    def __init__(self, source=None, metadata=dict()):
+        # Metadata can be filled as a dictionary before
+        # initialization. # Attributes which are not
+        # ExperimentalParameters are ignored.
+        metadata.update(
+            {"temperature": 100, "exposure": 1, "this_will_be_ignored": True}
+        )
+        super().__init__(source, metadata)
+
+        self.time_points = tuple(range(0, 3))
+        self.resolution = (64, 64)
+        self.scans = tuple(range(1, 3))
+        self.notes = "This is a test"
+        self.is_useful = False
+
+    def raw_data(self, *args, **kwargs):
+        return np.random.random(size=self.resolution)
---
+++ tests/test_plugins.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import unittest
+from pathlib import Path
+from tempfile import gettempdir
+
+from iris import AbstractRawDataset, DiffractionDataset
+from iris.plugins import load_plugin
+
+test_plugin_path = Path(__file__).parent / "plugin_fixture.py"
+
+class TestPlugin(unittest.TestCase):
+
+    def setUp(self):
+        load_plugin(test_plugin_path)
+    
+    def test_experimental_parameters(self):
+        """ Test that arbitrary experimental parameters can be manipulated """
+        from iris.plugins import TestRawDatasetPlugin
+        test = TestRawDatasetPlugin()
+        
+        test.is_useful = False
+        self.assertFalse(test.is_useful)
+
+        test.is_useful = True
+        self.assertTrue(test.is_useful)
+    
+    def test_reduction(self):
+        """ Test that data reduction works """
+        from iris.plugins import TestRawDatasetPlugin
+        test = TestRawDatasetPlugin()
+        test.scans = [1,2]
+        test.time_points = [-1,0,1]
+
+        temp_file = Path(gettempdir()) / "plugin_test.hdf5"
+        with DiffractionDataset.from_raw(test, filename=temp_file, mode='w') as dataset:
+            self.assertEqual(test.temperature, dataset.temperature)
+            # Assert that extra metadata is not kept
+            self.assertTrue(hasattr(test, "is_useful"))
+            self.assertFalse(hasattr(dataset, "is_useful"))
