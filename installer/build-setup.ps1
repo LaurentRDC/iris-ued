1b824dae5f5ab39ec1461672b60c58462016816e
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2019-10-10 15:21:16 -0400

    Building installer is now a two-step process
    Former-commit-id: 86ed5b13c0a45dc03d0afc71d9030b31b38d9260

--- installer/build-setup.ps1
+++ installer/build-setup.ps1
@@ -1,6 +1,3 @@
-Write-Host "Building .exe pyinstaller"
-python -O -m PyInstaller --clean -y --distpath=dist\executable --onedir iris-onedir.spec
-
 Write-Host "Building setup using ISCC"
 if ($ENV:PROCESSOR_ARCHITECTURE -eq "AMD64"){
     $iscc = get-item "C:\Program Files (x86)\Inno Setup 5\ISCC.exe"
